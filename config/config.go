ebc759081d7676b4f49eb292f6b55126307bbb5b
Habib Nurrahman <nurmanhabib@gmail.com>
2025-06-05 20:12:07 +0700

    feat: added config

--- config/config.go
+++ config/config.go
@@ -1,5 +1,26 @@
 package config
 
+import "go-api-cms/pkg/env"
+
 type Config struct {
-	DB Database
+	App App
+	DB  Database
+}
+
+type Option func(conf *Config)
+
+func New(options ...Option) *Config {
+	conf := &Config{
+		App: App{
+			Name:     env.Get("APP_NAME", "go-api-cms"),
+			Port:     env.GetInt("APP_PORT", 8080),
+			Timezone: env.Get("APP_TIMEZONE", "Asia/Jakarta"),
+		},
+	}
+
+	for _, option := range options {
+		option(conf)
+	}
+
+	return conf
 }
--- config/database.go
+++ config/database.go
@@ -1,9 +1,27 @@
 package config
 
+import "go-api-cms/pkg/env"
+
 type Database struct {
+	Driver   string
 	Host     string
 	Port     int
 	Username string
 	Password string
 	Database string
+	Timezone string
+}
+
+func WithDatabase() Option {
+	return func(conf *Config) {
+		conf.DB = Database{
+			Driver:   env.Get("DB_DRIVER", "postgres"),
+			Host:     env.Get("DB_HOST", "127.0.0.1"),
+			Port:     env.GetInt("DB_PORT", 5432),
+			Username: env.Get("DB_USERNAME", "root"),
+			Password: env.Get("DB_PASSWORD", ""),
+			Database: env.Get("DB_DATABASE", "go_api"),
+			Timezone: env.Get("DB_TIMEZONE", "Asia/Jakarta"),
+		}
+	}
 }
